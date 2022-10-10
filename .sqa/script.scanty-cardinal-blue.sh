393d15ec249831480f3108796d628df6a668d957
samuelbernardolip <44866834+samuelbernardolip@users.noreply.github.com>
2022-10-10 16:37:42 +0100

    Add JePL file structure

--- .sqa/script.scanty-cardinal-blue.sh
+++ .sqa/script.scanty-cardinal-blue.sh
@@ -1,4 +1,4 @@
 (
-cd github.com/tdviet4free/fedcloudclient &&
+cd github.com/EOSC-synergy/onedataSim &&
     bandit -f json --quiet  --confidence-level high --severity-level high --recursive .
 )
--- .sqa/script.tasty-aquamarine-antelope.sh
+++
@@ -1,4 +0,0 @@
-(
-cd github.com/tdviet4free/fedcloudclient &&
-    rst-lint --level warning --format json .
-)
