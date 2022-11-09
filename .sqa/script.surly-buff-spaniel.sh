a7bbe465dd924e8cdff0c5bffc061af191e65b22
orviz <orviz@ifca.unican.es>
2022-11-09 13:09:27 +0100

    Add JePL file structure

--- .sqa/script.surly-buff-spaniel.sh
+++
@@ -1,4 +0,0 @@
-(
-cd bitbucket.org/cessda/cessda.cdc.searchkit/src/master &&
-    npx stylelint --config-basedir . --config /root/.stylelintrc.json **/*.{js ts css vue}
-)
---
+++ .sqa/script.wheezy-rust-hornet.sh
@@ -0,0 +1,4 @@
+(
+cd bitbucket.org/cessda/cessda.cdc.searchkit/src/master &&
+    npx stylelint --config-basedir . --config /root/.stylelintrc.json **/*.{js,ts,css,vue}
+)
